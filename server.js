b8ed319344535bdad26452032b534597cda09905
Douaa <douaabounadar@gmail.com>
2026-03-10 03:34:21 +0000

    node calculator

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/add/:a/:b', (req,res)=>{
+    const a=parseInt(req.params.a);
+    const b=parseInt(req.params.b);
+    res.send((a+b).toString());
+});
+
+app.listen(3000,()=>{
+    console.log("Server running on port 3000");
+});
